9f2d268b06b1d27c6c3a758177e6fdc5e8f3372d
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-07-02 14:55:15 +0100

    message

--- Services/medlog/bookid.py
+++ Services/medlog/bookid.py
@@ -1,10 +1,10 @@
 """ generates random numner to be used as booking ID """
 
 from random import random
-from random import randint
+# from random import randint
 import math
 
 def generate_random_number():
-    r = random.randint(300, 900000)
+    r = random()
     return r
 print(generate_random_number())
