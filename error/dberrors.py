f04036db2ab9428d69f4f0a2a6fbc89ba4e3e461
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-07-08 12:18:08 +0100

    Another commit

--- error/dberrors.py
+++
@@ -1,4 +0,0 @@
-def generate_fake(count=100):
-    from sqlalchemy.exc import IntegrityError
-    from random import seed
-    import
--- fasttest.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-def generate_random_numbers():
-    id = uuid.uuid4()
-    return id.node
-print(generate_random_numbers())
