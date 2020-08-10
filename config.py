da4bdd212b269318c70ed9d28049430324b08877
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-08-10 13:59:50 +0100

    Last commit

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DEBUG = True
 
 # Globally database configuration
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-MONGOURI = f'mongodb+srv://{dbusername}:<{dbpassword}>@cluster0.2ip3w.mongodb.net/<hspcare>?retryWrites=true&w=majority'
+MONGOURI = f'mongodb+srv://{dbusername}:{dbpassword}@cluster0.2ip3w.mongodb.net/hspcare?retryWrites=true&w=majority'
 
 
 
--- instance/config.py
+++ instance/config.py
@@ -1,10 +1,10 @@
 import os
-# from instance.setins import sqlusername,sqlpassword
+from instance.setins import dbusername,dbpassword
 
 DEBUG = True
 
 # Globally database configuration
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-MONGOURI = 'mongodb://localhost:27017/forlog'
+MONGOURI = f'mongodb+srv://{dbusername}:{dbpassword}@cluster0.2ip3w.mongodb.net/hspcare?retryWrites=true&w=majority'
 
 
